dbeaa2e3d00a335e38111d93f2b099e3b77af759
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2016-03-29 01:26:42 +0200

    Example Projects

---
+++ AutoFacExample/AutoFacExample/Program.cs
@@ -0,0 +1,103 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+
+/*
+ *  FROM: http://autofac.readthedocs.org/en/latest/getting-started/index.html
+ *
+ */
+
+
+
+
+
+namespace AutoFacExample
+{
+    public interface IOutput
+    {
+        void Write(string content);
+    }
+
+    public class ConsoleOutput : IOutput
+    {
+        public void Write(string content)
+        {
+            Console.WriteLine(content);
+        }
+    }
+
+
+
+    public interface IDateWriter
+    {
+        void WriteDate();
+    }
+
+    public class TodayWriter : IDateWriter
+    {
+        private IOutput _output;
+        public TodayWriter(IOutput output)
+        {
+            this._output = output;
+        }
+
+        public void WriteDate()
+        {
+            this._output.Write(DateTime.Today.ToShortDateString());
+        }
+    }
+
+
+
+    class Program
+    {
+
+        private static IContainer Container { get; set; }
+
+        static void Main(string[] args)
+        {
+
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+            Container = builder.Build();
+
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+
+
+
+    //builder.Register(c => new LoggerConfiguration().WriteTo.ColoredConsole().CreateLogger());
+    //builder.RegisterType<SerilogOutput>().As<IOutput>();
+
+
+    public class SerilogOutput : IOutput
+    {
+        private ILogger _ilogger;
+
+        public SerilogOutput(ILogger logger)
+        {
+            _ilogger = logger;
+        }
+
+        public void Write(string content)
+        {
+            _ilogger.Information(content);
+        }
+    }
+
+
+}
---
+++ Remember-AutoFac/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using System.IO;
+
+
+/*
+ *  FROM: http://www.codeproject.com/Articles/25380/Dependency-Injection-with-Autofac
+ *
+ */
+
+
+
+
+namespace Remember
+{
+    class Program
+    {
+        static void Main()
+        {
+            var memos = new List<Memo> {
+                new Memo { Title = "Release Autofac 1.1", DueAt = new DateTime(2007, 03, 12) },
+                new Memo { Title = "Update CodeProject Article", DueAt = DateTime.Now },
+                new Memo { Title = "Release Autofac 3", DueAt = new DateTime(2011, 07, 01) }
+            };
+
+            var builder = new ContainerBuilder();
+            builder.Register(c => new MemoChecker(c.Resolve<IList<Memo>>(), c.Resolve<IMemoDueNotifier>()));
+            builder.RegisterType<PrintingNotifier>().As<IMemoDueNotifier>();
+            builder.RegisterInstance(memos).As<IList<Memo>>();
+            builder.RegisterInstance(Console.Out).As<TextWriter>().ExternallyOwned();
+
+            using (var container = builder.Build())
+            {
+                container.Resolve<MemoChecker>().CheckNow();
+            }
+
+            Console.WriteLine("Done! Press any key.");
+            Console.ReadKey();
+        }
+    }
+}
